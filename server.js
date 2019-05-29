bf71b03fb4309b45e7b76fc23834ff99adb0e8ff
shamali <thorveshamali@gmail.com>
2019-05-29 16:34:49 +0530

    added product

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
-
+const productRouter = require('./product');
 const app=express();
+app.use(productRouter);
 app.listen(3000,'0.0.0.0',() =>{
     console.log('server started on 3000');
 });
